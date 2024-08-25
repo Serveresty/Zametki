d09a71ccd1f4ca6348798f52d69da13b934142b5
Serveresty <cs.go.12228@gmail.com>
2024-08-25 22:23:16 +0300

    custom errors

---
+++ utils/custom-errors/custom_errors.go
@@ -0,0 +1,7 @@
+package customerrors
+
+import "errors"
+
+var ErrNoTokenFound = errors.New("no token found")
+var ErrInvalidToken = errors.New("invalid token")
+var ErrAlreadyRegistered = errors.New("already registered")

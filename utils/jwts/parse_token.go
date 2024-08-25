3c8e697480a8a6092ef312bff0111f82e0976ae4
Serveresty <cs.go.12228@gmail.com>
2024-08-25 22:23:58 +0300

    parse token method

--- utils/jwts/parse_token.go
+++ utils/jwts/parse_token.go
@@ -1,5 +1,28 @@
 package jwts
 
-func ParseToken(token string) {
+import (
+	"Zametki/configs"
+	"Zametki/models"
+	cerr "Zametki/utils/custom-errors"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
+)
+
+func ParseToken(token string) error {
+	if token == "" {
+		return cerr.ErrNoTokenFound
+	}
+
+	tokenString := strings.Split(token, " ")[1]
+
+	secretKey := []byte(configs.GetEnv("SECRET_KEY"))
+	tkn, err := jwt.ParseWithClaims(tokenString, &models.Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	if err != nil || !tkn.Valid {
+		return cerr.ErrInvalidToken
+	}
+
+	return nil
 }
